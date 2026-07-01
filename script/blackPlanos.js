7c495d64e4f6d1fd2320d36b28063f4e70dbbe7d
gonzxgx <iigust4vo@gmail.com>
2026-07-01 11:37:13 -0300

    update

--- script/blackPlanos.js
+++ script/blackPlanos.js
@@ -1,7 +1,7 @@
 /* /script/blackPlanos.js — V4 (três modos: 30dias | 60dias | reinicio) */
 
 (function () {
-  const DATA_URL = "./script/planos.json?v=v19-ilimitado-jul26";
+  const DATA_URL = "./script/planos.json?v=v21-ilimitado-jul26";
 
   let currentMode = null;
   let cache = null;
