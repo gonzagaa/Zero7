8660b1cf7997bd6a53abf2e56969d5fc8e5902e0
gonzxgx <iigust4vo@gmail.com>
2026-08-11 09:01:44 -0300

    update

--- script/blackPlanos.js
+++ script/blackPlanos.js
@@ -1,7 +1,7 @@
 /* /script/blackPlanos.js — V4 (três modos: 30dias | 60dias | reinicio) */
 
 (function () {
-  const DATA_URL = "./script/planos.json?v=v45-timer-quente-ago06";
+  const DATA_URL = "./script/planos.json?v=v46-timer-quente-ago06";
 
   let currentMode = null;
   let cache = null;
